c98de9cff9a67ea980c0a9afb2863059b16d77b2
Right Cramer <rightcramer@gmail.com>
2017-12-23 03:29:26 +0300

    Дичь

--- II/Form1.cs
+++ II/Form1.cs
@@ -12,34 +12,33 @@ namespace II
 {
     public partial class Form1 : Form
     {
-        double  LerningRate = 0.5,
-                Moment = 0.3;
+        double  LerningRate = 3,
+                Moment = 0.7;
 
         double  Deltaw1 = 0,
                 Deltaw2 = 0,
                 Deltaw3 = 0,
                 Deltaw4 = 0,
                 Deltaw5 = 0,
-                Deltaw6 = 0;
-
-        double  w1 = 0.45,
-                w2 = 0.78,
-                w3 = -0.12,
-                w4 = 0.13,
-                w5 = 1.5,
-                w6 = -2.3;
-
+                Deltaw6 = 0;                       
 
         int i, j;
-        int MaxEpoch = 501, TrainSet = 4;
+        int MaxEpoch = 500001, TrainSet = 4;
 
-        int ErrorValue = 0;
-        double ErrorBuff;
+        //int ErrorValue = 0;
+        //double ErrorBuff;
         double[] PredictionError = new double[4];
+        double sum = 0;
 
         public Form1()
         {
             InitializeComponent();
+            Synapse.w1 = 9.50281530957557;
+            Synapse.w2 = 0.970518993278058;
+            Synapse.w3 = 9.50154606154639;
+            Synapse.w4 = 0.961203246532013;
+            Synapse.w5 = 39.8237701124492;
+            Synapse.w6 = -49.6861620879739;
         }
 
         public double Sigmoid (double a)
@@ -47,12 +46,49 @@ namespace II
             return 1 / (1 + Math.Exp(-a));
         }
 
-        public double Error(double IdealResult, double Result)
+        public void ErrorArray(double IdealResult, double Result, int TrainSet)
         {
+            switch (TrainSet)
+            {
+                case 0:
+                    sum = 0;
+                    PredictionError[0] = Math.Pow(IdealResult - Result, 2);
+                    break;
+                case 1:
+                    PredictionError[1] = Math.Pow(IdealResult - Result, 2);
+                    break;
+                case 2:
+                    PredictionError[2] = Math.Pow(IdealResult - Result, 2);
+                    break;
+                case 3:
+                    PredictionError[3] = Math.Pow(IdealResult - Result, 2);
+                    break;
+            }
+            sum += PredictionError[TrainSet];
+            TrainSet++;
+            if (TrainSet == 4)
+            {
+                int i = 0;
+                while (i < PredictionError.Length)
+                {
+                    PredictionError[i] = 0;
+                    i++;
+                }
+
+            }
+        }
+        
+
+        public double Error()
+        {
+            return sum / 4; //4 - count трейнсетовs
+            /*
             PredictionError[ErrorValue] = Math.Pow(IdealResult - Result, 2);
             ErrorBuff += PredictionError[ErrorValue];
             ErrorValue++;
             return (ErrorBuff) / (ErrorValue);
+            */
+           
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -84,58 +120,54 @@ namespace II
                         n2 = 1;
                     }
 
-                    textBox1.Text = "" + n1;
-                    textBox2.Text = "" + n2;
+                    //textBox1.Text = "" + n1;
+                    //textBox2.Text = "" + n2;
                     //====================================
-                    textBox3.Text = Convert.ToString(w1);
-                    textBox4.Text = Convert.ToString(w2);
-                    textBox5.Text = Convert.ToString(w3);
-                    textBox6.Text = Convert.ToString(w4);
+                    //textBox3.Text = Convert.ToString(Synapse.w1);
+                    //textBox4.Text = Convert.ToString(Synapse.w2);
+                    //textBox5.Text = Convert.ToString(Synapse.w3);
+                    //textBox6.Text = Convert.ToString(Synapse.w4);
 
-                    textBox9.Text = Convert.ToString(w5);
-                    textBox10.Text = Convert.ToString(w6);
+                    //textBox9.Text = Convert.ToString(Synapse.w5);
+                    //textBox10.Text = Convert.ToString(Synapse.w6);
 
-                    double  s1 = Sigmoid(n1 * w1 + n2 * w3),
-                            s2 = Sigmoid(n1 * w2 + n2 * w4);
+                    double  s1 = Sigmoid(n1 * Synapse.w1 + n2 * Synapse.w3),
+                            s2 = Sigmoid(n1 * Synapse.w2 + n2 * Synapse.w4);
 
-                    textBox7.Text = Convert.ToString(s1);
-                    textBox8.Text = Convert.ToString(s2);
+                    //textBox7.Text = Convert.ToString(s1);
+                    //textBox8.Text = Convert.ToString(s2);
 
-                    double ResultSigmoid = Sigmoid(s1 * w5 + s2 * w6);
-                    textBox11.Text = Convert.ToString(ResultSigmoid);
+                    double ResultSigmoid = Sigmoid(s1 * Synapse.w5 + s2 * Synapse.w6);
+                    //textBox11.Text = Convert.ToString(ResultSigmoid);
 
-                    double ErrorPercent;
+                    //double ErrorPercent;
                     double MOR;
                     if (n1 + n2 == 1)
                     {
-                        ErrorPercent = Error(1, ResultSigmoid);
-                        MOR = 1 - ResultSigmoid;
+                        ErrorArray(1, ResultSigmoid, j);
+                        MOR = (1 - ResultSigmoid) * (1 - ResultSigmoid) * ResultSigmoid;
                     }
                     else
                     {
-                        ErrorPercent = Error(0, ResultSigmoid);
-                        MOR = 0 - ResultSigmoid;
+                        ErrorArray(0, ResultSigmoid, j);
+                        MOR = (0 - ResultSigmoid) * (1 - ResultSigmoid) * ResultSigmoid;
                     }
 
-                    textBox12.Text = Convert.ToString(ErrorPercent + "%");
+                    //textBox13.Text = Convert.ToString(MOR);
 
-                    MOR *= (1 - MOR) * MOR;
+                    double  DeltaH1 = ((1 - s1) * s1) * (Synapse.w5 * MOR),
+                            DeltaH2 = ((1 - s2) * s2) * (Synapse.w6 * MOR);
 
-                    textBox13.Text = Convert.ToString(MOR);
+                    //textBox14.Text = Convert.ToString(DeltaH1);
+                    //textBox15.Text = Convert.ToString(DeltaH2);
 
-                    double  DeltaH1 = ((1 - s1) * s1) * (w5 * MOR),
-                            DeltaH2 = ((1 - s2) * s2) * (w6 * MOR);
-
-                    textBox14.Text = Convert.ToString(DeltaH1);
-                    textBox15.Text = Convert.ToString(DeltaH2);
-
-                    double Gradw5 = s1 * MOR,
+                    double  Gradw5 = s1 * MOR,
                             Gradw6 = s2 * MOR;
 
-                    Deltaw5 = LerningRate * Gradw5 + Deltaw5 * Moment;
-                    Deltaw6 = LerningRate * Gradw6 + Deltaw6 * Moment;
-                    w5 += Deltaw5;
-                    w6 += Deltaw6;
+                    //Deltaw5 = LerningRate * Gradw5 + Deltaw5 * Moment;
+                    //Deltaw6 = LerningRate * Gradw6 + Deltaw6 * Moment;
+                    Synapse.w5 += Deltaw5;
+                    Synapse.w6 += Deltaw6;
 
                     double  Gradw1 = n1 * DeltaH1,
                             Gradw2 = n1 * DeltaH2,
@@ -146,12 +178,13 @@ namespace II
                     Deltaw2 = LerningRate * Gradw2 + Deltaw2 * Moment;
                     Deltaw3 = LerningRate * Gradw3 + Deltaw3 * Moment;
                     Deltaw4 = LerningRate * Gradw4 + Deltaw4 * Moment;
-
-                    w1 += Deltaw1;
-                    w2 += Deltaw2;
-                    w3 += Deltaw3;
-                    w4 += Deltaw4;
-
+                    
+                    Synapse.w1 += Deltaw1;
+                    Synapse.w2 += Deltaw2;
+                    Synapse.w3 += Deltaw3;
+                    Synapse.w4 += Deltaw4;
+                    
+                    /*
                     textBox1.Refresh();
                     textBox2.Refresh();
                     textBox3.Refresh();
@@ -167,15 +200,29 @@ namespace II
                     textBox13.Refresh();
                     textBox14.Refresh();
                     textBox15.Refresh();
+                    */
                     //System.Threading.Thread.Sleep(500);
                     //====================================
+                    Console.WriteLine(String.Format("{0} {1} Сигмоида ответа: {2}",n1, n2, ResultSigmoid));
                 }
+                /*
+                Synapse.w1 = w1;
+                Synapse.w2 = w2;
+                Synapse.w3 = w3;
+                Synapse.w4 = w4;
+                Synapse.w5 = w5;
+                Synapse.w6 = w6;
+                */
+                //Error();
+                textBox12.Text = Convert.ToString(Error() + "%");
+                textBox12.Refresh();
                 j = 0;
-                ErrorBuff = 0;
-                ErrorValue = 0;
+                //ErrorBuff = 0;
+                //ErrorValue = 0;
                 label9.Text = "" + i;
                 label9.Refresh();
             }
+            Console.WriteLine(String.Format("{0} {1} {2} {3} {4} {5}", Synapse.w1, Synapse.w2, Synapse.w3, Synapse.w4, Synapse.w5, Synapse.w6));
         }
     }
 }
---
+++ II/Synapse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace II
+{
+    static class Synapse
+    {
+        public static double w1 { get; set; }
+        public static double w2 { get; set; }
+        public static double w3 { get; set; }
+        public static double w4 { get; set; }
+        public static double w5 { get; set; }
+        public static double w6 { get; set; }
+    }
+}
